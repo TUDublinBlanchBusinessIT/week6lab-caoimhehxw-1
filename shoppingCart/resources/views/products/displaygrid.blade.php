224803485d7762dafac65c7b37bafead7063312d
B00153330 Caoimhe Walsh <B00153330@mytudublin.ie>
2025-03-03 16:31:02 +0000

    third step commit

--- shoppingCart/resources/views/products/displaygrid.blade.php
+++ shoppingCart/resources/views/products/displaygrid.blade.php
@@ -6,7 +6,7 @@
             <li class="nav-item"><button id="checkOut" onclick="window.location.href=''" type="button" style="margin-right:5px;" class="btn btn-primary navbar-btn center-block">Check Out</button></li>
             <li class="nav-item"><button id="emptycart" type="button" style="margin-right:5px;" class="btn btn-primary navbar-btn center-block">Empty Cart</button></li>
             <li class="nav-item"><span style="font-size:40px;margin-right:0px;" class="glyphicon glyphicon-shopping-cart navbar-btn"></span></li>
-            <li class="nav-item"><div class="navbar-text" id="shoppingcart" style="font-size:12pt;margin-left:5px;margin-right:0px;"></div></li>
+            <li class="nav-item"><div class="navbar-text" id="shoppingcart" style="font-size:12pt;margin-left:5px;margin-right:0px;">$totalitems</div></li>
             <li class="nav-item"><div class="navbar-text" style="font-size:14pt;margin-left:0px;">Item(s)</div></li>
         <ul>
     </nav>
@@ -40,6 +40,16 @@
           alert("problem communicating with the server");
       }
     });
+});
+$("#emptycart").click(function() { $.ajax({
+    type: "get", url: "{{ url('products/emptycart')   }}",
+    success: function() {
+        $('#shoppingcart').text(0);
+    },
+    error: function() {
+        alert("problem communicating with the server");
+    }
+  });
 });
 </script>
 
